4f22ff1265f35a001ffdb9d7d0071fd31c1bf805
Lokman Razak <lokman.razak@me.com>
2020-12-06 19:04:50 +0000

    Implement InvitationListCreator and add tests

--- src/main/java/InvitationListCreator.java
+++ src/main/java/InvitationListCreator.java
@@ -1,13 +1,29 @@
 package com.lokmanrazak.main.java;
 
-import com.lokmanrazak.main.java.utilities.DistanceCalculator;
-import com.lokmanrazak.main.java.utilities.JsonReader;
+import com.lokmanrazak.main.java.interfaces.JsonHandler;
+import com.lokmanrazak.main.java.models.Customer;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InvitationListCreator {
-    private JsonReader jsonReader;
-    private DistanceCalculator distanceCalculator;
+    private JsonHandler jsonHandler;
 
-    public InvitationListCreator() {
+    public InvitationListCreator(JsonHandler jsonHandler) {
+        this.jsonHandler = jsonHandler;
+    }
 
+    public void getList(String filePath, double distance) {
+        List<Customer> customers = jsonHandler.getCustomerList(filePath);
+
+        List<Customer> filteredCustomers =
+                customers
+                        .stream()
+                        .filter(customer -> customer.distance <= distance)
+                        .sorted(Comparator.comparing(customer -> customer.userId))
+                        .collect(Collectors.toList());
+
+        jsonHandler.outputFile(filePath, filteredCustomers);
     }
 }
---
+++ src/test/java/InvitationListCreatorTests.java
@@ -0,0 +1,55 @@
+package com.lokmanrazak.test.java;
+
+import com.lokmanrazak.main.java.InvitationListCreator;
+import com.lokmanrazak.main.java.interfaces.JsonHandler;
+import com.lokmanrazak.main.java.models.Customer;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class InvitationListCreatorTests {
+    @Test
+    public void getList_validArguments_returnCorrectResult() {
+        ArgumentCaptor<List<Customer>> captor = ArgumentCaptor.forClass((Class) List.class);
+
+        JsonHandler jsonHandlerMock = mock(JsonHandler.class);
+        InvitationListCreator ilc = new InvitationListCreator(jsonHandlerMock);
+
+        List<Customer> customers = Arrays.asList(
+                createCustomer(2, "Michael Jordan", 150),
+                createCustomer(3, "Terry Ford", 65),
+                createCustomer(1, "Laura Big", 27)
+        );
+
+        when(jsonHandlerMock.getCustomerList(anyString())).thenReturn(customers);
+
+        ilc.getList("customers.txt", 100);
+
+        verify(jsonHandlerMock).getCustomerList(anyString());
+        verify(jsonHandlerMock).outputFile(anyString(), captor.capture());
+
+        List<Customer> value = captor.getValue();
+
+        assertEquals(value.size(), 2);
+
+        assertEquals(value.get(0).userId, 1);
+        assertEquals(value.get(0).name, "Laura Big");
+
+        assertEquals(value.get(1).userId, 3);
+        assertEquals(value.get(1).name, "Terry Ford");
+    }
+
+    private Customer createCustomer(int userId, String name, double distance) {
+        Customer customer = new Customer();
+        customer.userId = userId;
+        customer.name = name;
+        customer.distance = distance;
+        return customer;
+    }
+}
